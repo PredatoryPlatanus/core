30c76d9e94e628c2343fcf91e053241c33a51b4a
Dmytro Onypko <vaporif@gmail.com>
2017-04-22 23:11:17 +0300

    initial

---
+++ App.Db/AppContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using App.Db.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Db
+{
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=AppDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+    }
+}
---
+++ App.Db/Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App.Common;
+
+namespace App.Db.Models
+{
+    public class WeatherForecast : Entity
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+    }
+}
